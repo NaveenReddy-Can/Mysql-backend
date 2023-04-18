599d530847d0943b858f48422c72d8211b2fe4f2
Naveen Reddy <nrkb1998@gmail.com>
2023-04-18 11:43:21 -0400

    added stripe code

--- app.js
+++ app.js
@@ -15,13 +15,34 @@ const port = 8001;
 
 
 // middleware
+// This is your test secret API key.
+const stripe = require("stripe")("sk_test_51MTojlHAH0Gg4pVKiC0YFhn2vvbBKrgS1wuFGD51nkizL9JLo6sDgYV0dDHr29BC2Bx9kpBHuGs05AZPncEG2dQf00pb0RxH6x");
+
+app.use(express.static("public"));
 app.use(express.json())
 app.use(cors());
 
 app.use(router);
 
 
+const calculateOrderAmount = items => {
 
-app.listen(port,()=>{
+    return 1000;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body;
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "CAD"
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    });
+});
+
+app.listen(port, () => {
     console.log("server starts at port no :" + port);
 })
